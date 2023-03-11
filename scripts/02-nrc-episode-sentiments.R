d0b1f4f2578de038511507d730acb148baea259a
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2023-03-11 10:05:13 -0500

    Update 02-nrc-episode-sentiments.R

--- scripts/02-nrc-episode-sentiments.R
+++ scripts/02-nrc-episode-sentiments.R
@@ -7,7 +7,7 @@ pacman::p_load(dplyr,
                textdata)
 
 # load data
-scripts <- read_csv("scripts.csv") %>%
+scripts <- read_csv("raw-data/scripts.csv") %>%
   janitor::clean_names()
 
 # wrangle data
@@ -48,4 +48,4 @@ mean_episode_sentiments <- db_nrc %>%
   summarise(mean_sent = round(mean(sentiment),2))
 
 # export episode sentiments
-write_csv(mean_episode_sentiments, "02-nrc-episode-sentiments.csv")
+write_csv(mean_episode_sentiments, "data/02-nrc-episode-sentiments.csv")

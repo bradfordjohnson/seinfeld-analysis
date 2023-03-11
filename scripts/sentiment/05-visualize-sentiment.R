2a093a4aa7587050a0767eb8d0b8b63777e4a9c7
Ford Johnson <bradford.lewis.johnson@outlook.com>
2023-03-11 13:09:10 -0500

    added basic visual for sentiment #2

---
+++ scripts/sentiment/05-visualize-sentiment.R
@@ -0,0 +1,17 @@
+# load packages
+pacman::p_load(tidyverse)
+
+# load data
+lex <- read_csv("data/sentiment/04-combined-lex.csv")
+
+# wrangle data
+lex_means <- lex %>%
+  group_by(lex) %>%
+  summarise(mean = mean(mean_sent))
+
+# visualize
+lex %>%
+  ggplot(aes(x = show_number, y = mean_sent)) +
+  geom_col() +
+  facet_wrap(~lex) +
+  geom_hline(data = lex_means, aes(yintercept = mean))

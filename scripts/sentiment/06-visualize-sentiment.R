12eefac86e948a0c0a8308d7a7001300404a5a3b
Ford Johnson <bradford.lewis.johnson@outlook.com>
2023-03-12 01:54:10 -0500

    updated raincloud plot

--- scripts/sentiment/06-visualize-sentiment.R
+++ scripts/sentiment/06-visualize-sentiment.R
@@ -2,15 +2,33 @@
 pacman::p_load(tidyverse,
                tidyquant,
                ggdist,
-               ggthemes)
+               ggthemes,
+               ggtext)
 
 # load data
 lex <- read_csv("data/sentiment/04-combined-lex.csv")
 
+# fix case
+lex <- lex %>%
+  mutate(lex = case_when(
+    lex == "nrc" ~ "NRC",
+    lex == "bing" ~ "Bing",
+    lex == "afinn" ~ "AFINN"
+  ))
+
 # visualize sentiment
 lex %>%
-  ggplot(aes(x = lex, y = mean_sent, fill = factor(lex), color = factor(lex))) +
-  geom_hline(yintercept = 0, color = "gray30", linetype = "dashed") +
+  ggplot(
+    aes(x = lex,
+        y = mean_sent,
+        fill = factor(lex),
+        color = factor(lex))
+    ) +
+  geom_hline(
+    yintercept = 0,
+    color = "#82889B",
+    linetype = "dashed"
+    ) +
   stat_halfeye(
     # adjust bandwidth
     adjust = 0.5,
@@ -32,27 +50,96 @@ lex %>%
     # adjusting position
     justification = 1.1
   ) +
-  scale_fill_manual(values = c("#000000","#000000","#000000")) +
-  scale_color_manual(values = c("#000000","#000000","#000000")) +
+  scale_fill_manual(
+    values = c(
+      "#A1566E",
+      "#DF7C6C",
+      "#FFB462"
+      )
+    ) +
+  scale_color_manual(
+    values = c(
+      "#A1566E",
+      "#DF7C6C",
+      "#FFB462"
+      )
+    ) +
+  scale_y_continuous(
+    limits = c(-1.5,1.5)
+    ) +
   theme_tq() +
   coord_flip() +
-  geom_segment(aes(x = 0, y = 0, xend = 0, yend = .25),
-               arrow = arrow(length = unit(2,"mm"), type = "closed")) +
-  geom_segment(aes(x = 0, y = 0, xend = 0, yend = -.25),
-               arrow = arrow(length = unit(2,"mm"), type = "closed")) +
   geom_text(
-    aes(x = 0, y = .42),
-    label = "More Positive",
-    color = "green"
+    aes(x = .01, y = 1),
+    label = "Positive",
+    color = "#82889B"
     ) +
   geom_text(
-    aes(x = 0, y = -.43),
-    label = "More Negative",
-    color = "red"
+    aes(x = .01, y = -1),
+    label = "Negative",
+    color = "#82889B"
+    ) +
+  geom_text(
+    aes(x = 1, y = -1),
+    label = "AFINN",
+    color = "#A1566E"
   ) +
+  geom_text(
+    aes(x = 2, y = -1),
+    label = "Bing",
+    color = "#DF7C6C"
+  ) +
+  geom_text(
+    aes(x = 3, y = -1),
+    label = "NRC",
+    color = "#FFB462"
+  ) +
+  geom_curve(
+    aes(
+      x = 2, y = .7, xend = 1.87, yend = .49
+      ),
+    arrow = arrow(length = unit(2,"mm"),
+      type = "closed"
+      ),
+    color = "#82889B",
+    curvature = -.5
+    ) +
+  geom_text(
+    aes(x = 2.15, y = .82),
+    label = "Single Episode",
+    color = "#82889B"
+    ) +
+  labs(
+    y = "Sentiment",
+    title = "  
+Seinfeld Script Analysis
+       ",
+    subtitle = "<span style='color:#82889B'>Seinfeld had 180 episodes during the  
+    9 seasons it aired on TV. Featuring  
+    over 54,000 lines of text in the script.
+    ",
+  )+
   theme(
     legend.position = "none",
     panel.grid = element_blank(),
     panel.border = element_blank(),
-    axis.ticks = element_blank()
-  )
+    axis.ticks = element_blank(),
+    plot.background = element_rect(fill = "#1F2133",
+                                   colour = "#1F2133"),
+    panel.background = element_rect(fill = "#1F2133",
+                                    colour = "#1F2133"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(color = "#82889B"),
+    axis.title.y = element_blank(),
+    axis.title.x = element_text(color = "#82889B"),
+    axis.line.x = element_line(colour = "#82889B",
+                               arrow = arrow(
+                                 length = unit(2,"mm"),
+                                 type = "closed",
+                                 ends = "both"
+                                 )
+                               ),
+    plot.title = element_text(color = "#82889B"),
+    plot.subtitle = element_markdown(hjust = 0, size = 9, lineheight = 1)
+)
+
